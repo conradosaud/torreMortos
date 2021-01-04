6fd32740257bf002ab5a865524123434ca7f7efc
Conrado Saud <conradosaud@gmail.com>
2021-01-04 12:16:36 -0300

    Andar1 - entrada, completo!
    Completinho, falta só adicionar os inimigos e as interações

---
+++ unity/TorreMortos/Assets/cenario/Entrada/Buraco/Buraco.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Buraco : MonoBehaviour
+{
+
+    public Collider chao;
+
+    private void OnTriggerEnter(Collider other) {
+        chao.enabled = false;
+    }
+    private void OnTriggerExit(Collider other) {
+        chao.enabled = true;
+    }
+
+}
